f1c6d47140bbe4b4c8dac1b486362d2f62c319b1
alen4203 <alen4203@gmail.com>
2021-08-30 00:04:17 +0800

    Almost done

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = amount * rate * years /100;
-    alert(interest);
+    var date = new Date();
+    var targetYear = Number(date.getFullYear()) + Number(years);
+    document.getElementById("result").innerHTML="If you deposit " + amount + " ,<br>at an interest rate of "
+                                                + rate + "%.<br>You will receive an amount of " + interest +
+                                                ",<br>in the year " + targetYear + ".";
 }
 
-function show_rate()
+function showRate()
 {
     var rate = document.getElementById("rate").value;
     var show = document.getElementById("rate_val");
