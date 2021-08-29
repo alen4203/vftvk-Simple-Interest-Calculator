e466471d2770526ab3c02426bf4d4d78304e8576
alen4203 <alen4203@gmail.com>
2021-08-29 20:23:15 +0800

    Second update

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var amount = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = amount * rate * years /100;
+    alert(interest);
+}
+
+function show_rate()
+{
+    var rate = document.getElementById("rate").value;
+    var show = document.getElementById("rate_val");
+    show.innerHTML= rate;
     
 }
         
